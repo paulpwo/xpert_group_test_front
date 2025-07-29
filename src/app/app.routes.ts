7793d881458452ca568615ab655bc23cea2e99bd
Paul Osinga <paulpwo@gmail.com>
2025-07-29 14:49:18 -0500

    feat: add breeds table route to application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { BreedsTableComponent } from './cats/breeds-table/breeds-table.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'breeds-table', component: BreedsTableComponent }
 ];
